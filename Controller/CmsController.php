8a1c59bcb41a06625bc03527dbabada1b3df9ad5
Weida <weida@gravitate.co.nz>
2019-04-15 10:31:30 +1200

    fix controller

--- Controller/CmsController.php
+++ Controller/CmsController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Tatooine\Controller;
+namespace MillenniumFalcon\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
