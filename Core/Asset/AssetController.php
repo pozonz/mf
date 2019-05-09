757dd06a765fe16358d71ba9bb313c84e5e0b5e4
Weida <weida@gravitate.co.nz>
2019-05-09 19:42:07 +1200

    automate crop

--- Core/Asset/AssetController.php
+++ Core/Asset/AssetController.php
@@ -14,10 +14,11 @@ use Symfony\Component\Routing\Annotation\Route;
 class AssetController extends Controller
 {
     /**
-     * @Route("/images/assets/{assetCode}/{assetSizeCode}/", name="asset_image", methods={"GET"})
+     * @Route("/images/assets/{assetCode}", name="asset_image_original", methods={"GET"})
+     * @Route("/images/assets/{assetCode}/{assetSizeCode}", name="asset_image", methods={"GET"})
      * @Route("/images/assets/{assetCode}/{assetSizeCode}/{fileName}", name="asset_image_filename", methods={"GET"})
      */
-    public function assetImage($assetCode, $assetSizeCode, $fileName = null)
+    public function assetImage($assetCode, $assetSizeCode = null, $fileName = null)
     {
         $request = Request::createFromGlobals();
         $useWebp = in_array('image/webp', $request->getAcceptableContentTypes());
@@ -32,41 +33,43 @@ class AssetController extends Controller
             throw new NotFoundHttpException();
         }
 
-        /** @var AssetSize $assetSize */
-        $assetSize = AssetSize::getByField($pdo, 'code', $assetSizeCode);
-        if (!$assetSize) {
-            throw new NotFoundHttpException();
-        }
-
-        $cachedFolder = $this->container->getParameter('kernel.project_dir') . '/cache/image/';
-        if (!file_exists($cachedFolder)) {
-            mkdir($cachedFolder, 0777, true);
-        }
-
         $uploadPath = $this->container->getParameter('kernel.project_dir') . '/uploads/';
-
         $fileType = $asset->getFileType();
         $fileName = $asset->getFileName();
         $fileSize = $asset->getFileSize();
         $fileExtension = $asset->getFileExtension();
         $fileLocation = $uploadPath . $asset->getFileLocation();
 
+        if ($assetSizeCode) {
+            /** @var AssetSize $assetSize */
+            $assetSize = AssetSize::getByField($pdo, 'code', $assetSizeCode);
+            if (!$assetSize) {
+                throw new NotFoundHttpException();
+            }
 
+            $cachedFolder = $this->container->getParameter('kernel.project_dir') . '/cache/image/';
+            if (!file_exists($cachedFolder)) {
+                mkdir($cachedFolder, 0777, true);
+            }
 
-        if ('application/pdf' == $fileType) {
+            if ('application/pdf' == $fileType) {
 
-        } elseif ($useWebp) {
-            $thumbnail = $cachedFolder . md5($asset->getId() . '-' . $assetSize->getId() . '-' . $assetSize->getWidth()) . "-webp.webp";
-            $command = getenv('CWEBP_CMD') . ' ' . $fileLocation . ' -resize ' . $assetSize->getWidth() . ' 0  -o ' . $thumbnail;
+            } elseif ($useWebp) {
+                $thumbnail = $cachedFolder . md5($asset->getId() . '-' . $assetSize->getId() . '-' . $assetSize->getWidth()) . "-webp.webp";
+                $command = getenv('CWEBP_CMD') . ' ' . $fileLocation . ' -resize ' . $assetSize->getWidth() . ' 0  -o ' . $thumbnail;
 
+            } else {
+                $thumbnail = $cachedFolder . md5($asset->getId() . '-' . $assetSize->getId() . '-' . $assetSize->getWidth()) . ".{$asset->getFileExtension()}";
+                $command = getenv('CONVERT_CMD') .  ' ' . $fileLocation . ' -quality 95 -resize ' . $assetSize->getWidth() .'x ' . $thumbnail;
+            }
+
+            if (!file_exists($thumbnail)) {
+                $returnValue = $this->generateOutput($command);
+            }
         } else {
-            $thumbnail = $cachedFolder . md5($asset->getId() . '-' . $assetSize->getId() . '-' . $assetSize->getWidth()) . ".{$asset->getFileExtension()}";
-            $command = getenv('CONVERT_CMD') .  ' ' . $fileLocation . ' -quality 95 -resize ' . $assetSize->getWidth() .'x ' . $thumbnail;
+            $thumbnail = $fileLocation;
         }
 
-        if (!file_exists($thumbnail)) {
-            $returnValue = $this->generateOutput($command);
-        }
 
         $date = new \DateTimeImmutable('@' . filectime($uploadPath));
         $saveDate = $date->setTimezone(new \DateTimeZone("GMT"))->format("D, d M y H:i:s T");
