88315bcd96725c2abf537b9f9256b5f1eb22d157
Darth Vader <pozoltd@gmail.com>
2019-08-06 20:18:20 +1200

    snippet

--- Core/Orm/Traits/ContentSnippetTrait.php
+++ Core/Orm/Traits/ContentSnippetTrait.php
@@ -4,4 +4,16 @@ namespace MillenniumFalcon\Core\Orm\Traits;
 
 trait ContentSnippetTrait
 {
+    /**
+     * @return mixed
+     */
+    public function objContent()
+    {
+        $result = [];
+        $objContent = json_decode($this->getContent());
+        foreach ($objContent as $itm) {
+            $result[$itm->attr] = $itm;
+        }
+        return $result;
+    }
 }
