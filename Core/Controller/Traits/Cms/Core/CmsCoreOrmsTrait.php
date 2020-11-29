ca9cbe7e8b2ad674d79aa68956b83d9483a6a56a
Weida <weida@gravitate.co.nz>
2020-11-29 20:17:46 +1300

    fix problem

--- Core/Controller/Traits/Cms/Core/CmsCoreOrmsTrait.php
+++ Core/Controller/Traits/Cms/Core/CmsCoreOrmsTrait.php
@@ -2,9 +2,16 @@
 
 namespace MillenniumFalcon\Core\Controller\Traits\Cms\Core;
 
+use App\ORM\Order;
+use App\ORM\OrderItem;
 use BlueM\Tree;
 use MillenniumFalcon\Core\ORM\_Model;
+use MillenniumFalcon\Core\ORM\FormDescriptor;
+use MillenniumFalcon\Core\ORM\FormSubmission;
 use MillenniumFalcon\Core\Service\ModelService;
+use PhpOffice\PhpSpreadsheet\Cell\Coordinate;
+use PhpOffice\PhpSpreadsheet\IOFactory;
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use MillenniumFalcon\Core\Tree\RawData;
@@ -30,6 +37,148 @@ trait CmsCoreOrmsTrait
 
         return $this->render($params['theNode']->template, $params);
     }
+
+    /**
+     * @route("/manage/orms/FormSubmission")
+     * @route("/manage/admin/orms/FormSubmission")
+     * @return Response
+     */
+    public function formSubmissions(Request $request)
+    {
+        $params = $this->getCmsTemplateParams($request);
+
+        $fullClass = ModelService::fullClass($this->connection, 'FormDescriptor');
+        $params['formDescriptors'] = $fullClass::active($this->connection, [
+            'sort' => 'm.title'
+        ]);
+        $params['formDescriptors'] = array_map(function ($itm) {
+            $fullClass = ModelService::fullClass($this->connection, 'FormSubmission');
+            $total = $fullClass::data($this->connection, [
+                'count' => 1,
+                'whereSql' => 'm.formDescriptorId = ?',
+                'params' => [$itm->getId()],
+            ]);
+            $itm->_count = $total['count'];
+            return $itm;
+        }, $params['formDescriptors']);
+
+        $params['filterFormDescriptor'] = $fullClass::getBySlug($this->connection, $request->get('form'));
+        $params['filterStart'] = $request->get('start');
+        $params['filterEnd'] = $request->get('end');
+
+
+        $model = _Model::getByField($this->connection, 'className', 'FormSubmission');
+        $params['ormModel'] = $model;
+
+        $fullClass = ModelService::fullClass($this->connection, $model->getClassName());
+
+        $pageNum = $request->get('pageNum') ?: 1;
+        $sort = $request->get('sort') ?: $model->getDefaultSortBy();
+        $order = $request->get('order') ?: ($model->getDefaultOrder() == 0 ? 'ASC' : 'DESC');
+        $keyword = $request->get('keyword') ?: '';
+        $status = $request->get('status') ?: 0;
+
+
+        $sqlWhere = '';
+        $sqlParams = [];
+
+        if ($params['filterFormDescriptor']) {
+            $sqlWhere .= ($sqlWhere ? ' AND ' : '') . '(m.formDescriptorId = ?)';
+            $sqlParams = array_merge($sqlParams, [
+                $params['filterFormDescriptor']->getId(),
+            ]);
+        }
+
+        if ($params['filterStart']) {
+            $sqlWhere .= ($sqlWhere ? ' AND ' : '') . '(m.added >= ?)';
+            $sqlParams = array_merge($sqlParams, [
+                date('Y-m-d 00:00:00', strtotime($params['filterStart']))
+            ]);
+        }
+
+        if ($params['filterEnd']) {
+            $sqlWhere .= ($sqlWhere ? ' AND ' : '') . '(m.added <= ?)';
+            $sqlParams = array_merge($sqlParams, [
+                date('Y-m-d 23:59:59', strtotime($params['filterEnd']))
+            ]);
+        }
+
+        $export = $request->get('export');
+        if ($params['filterFormDescriptor'] && $export) {
+            /** @var FormSubmission[] $orms */
+            $orms = $fullClass::data($this->connection, [
+                "whereSql" => $sqlWhere,
+                "params" => $sqlParams,
+                "sort" => $sort,
+                "order" => $order,
+            ]);
+
+            $phpExcel = new Spreadsheet();
+            $sheet = $phpExcel->getActiveSheet();
+
+            /** @var FormDescriptor $filterFormDescriptor */
+            $filterFormDescriptor = $params['filterFormDescriptor'];
+
+            $formFileds = json_decode($filterFormDescriptor->getFormFields());
+
+            $count = 1;
+            $sheet->setCellValue(Coordinate::stringFromColumnIndex(1) . "{$count}", 'Date');
+
+            foreach ($formFileds as $idx => $itm) {
+                $sheet->setCellValue(Coordinate::stringFromColumnIndex($idx + 2) . "{$count}", $itm->label);
+            }
+
+            $count = 2;
+            foreach ($orms as $formSubmission) {
+                $sheet->setCellValue(Coordinate::stringFromColumnIndex(1) . "{$count}", date('d M Y@H:i:s', strtotime($formSubmission->getAdded())));
+
+                $jsonContent = json_decode($formSubmission->getContent());
+                foreach ($jsonContent as $idx => $itm) {
+                    if ($itm[0] == 'antispam') {
+                        continue;
+                    }
+                    if (gettype($itm[1]) == 'array') {
+                        $itm[1] = implode(',', $itm[1]);
+                    }
+                    $sheet->setCellValue(Coordinate::stringFromColumnIndex($idx + 2) . "{$count}", $itm[1]);
+                }
+                $count++;
+            }
+
+            $writer = IOFactory::createWriter($phpExcel, 'Xlsx');
+            header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+            header('Content-Disposition: attachment; filename="' . $filterFormDescriptor->getSlug() . '-export-' . date('Y-m-d-H-i-s') . '.xlsx' . '"');
+            $writer->save('php://output');
+            exit;
+        }
+
+        $orms = $fullClass::data($this->connection, [
+            "whereSql" => $sqlWhere,
+            "params" => $sqlParams,
+            "page" => $pageNum,
+            "limit" => $model->getNumberPerPage(),
+            "sort" => $sort,
+            "order" => $order,
+        ]);
+
+        $total = $fullClass::data($this->connection, [
+            "whereSql" => $sqlWhere,
+            "params" => $sqlParams,
+            "count" => 1,
+        ]);
+
+        $params['total'] = $total['count'];
+        $params['totalPages'] = ceil($total['count'] / $model->getNumberPerPage());
+        $params['url'] = $request->getPathInfo() . "?sort=$sort&order=$order&form={$request->get('form')}&start={$request->get('start')}&end={$request->get('end')}";
+        $params['urlNoSort'] = $request->getPathInfo();
+        $params['pageNum'] = $pageNum;
+        $params['sort'] = $sort;
+        $params['order'] = $order;
+        $params['orms'] = $orms;
+
+        return $this->render($params['theNode']->template, $params);
+    }
+
 
     /**
      * @Route("/manage/orms/Redirect")
