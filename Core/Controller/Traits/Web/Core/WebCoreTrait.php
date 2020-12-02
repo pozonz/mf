72f59fecc4d469aae20952fe0550608e3cdc4917
Darth Vader <pozoltd@gmail.com>
2020-12-02 21:01:26 +1300

    sitemap

--- Core/Controller/Traits/Web/Core/WebCoreTrait.php
+++ Core/Controller/Traits/Web/Core/WebCoreTrait.php
@@ -11,52 +11,51 @@ use Symfony\Component\Routing\Annotation\Route;
 
 trait WebCoreTrait
 {
-//    /**
-//     * @route("/sitemap.xml")
-//     * @return Response
-//     */
-//    public function sitemap()
-//    {
-//        $sitemap = [];
-//        $request = Request::createFromGlobals();
-//        $this->connection = $this->container->get('doctrine.dbal.default_connection');
-//        $fullClass = ModelService::fullClass($this->connection, "Page");
-//        $orms = $fullClass::active($this->connection, [
-//            'whereSql' => '(m.hideFromWebNav != 1 OR m.hideFromWebNav IS NULL)'
-//        ]);
-//        foreach ($orms as $orm) {
-//            $sitemap[] = [
-//                'url' => $request->getSchemeAndHttpHost() . $orm->getUrl(),
-//            ];
-//        }
-//
-//        /** @var _Model[] $models */
-//        $models = _Model::active($this->connection);
-//        foreach ($models as $model) {
-//            if ($model->getSiteMapUrl()) {
-//                $url = $model->getSiteMapUrl();
-//                $fullClass = ModelService::fullClass($this->connection, $model->getClassName());
-//                $fields = array_keys($fullClass::getFields());
-//                $orms = $fullClass::active($this->connection);
-//                foreach ($orms as $orm) {
-//                    $ormUrl = $url;
-//                    foreach ($fields as $field) {
-//                        $attr = 'get' . ucfirst($field);
-//                        $replace = '{' . $field . '}';
-//                        $value = $orm->$attr();
-//                        $ormUrl = str_replace($replace, $value, $ormUrl);
-//                    }
-//                    $sitemap[] = [
-//                        'url' => $request->getSchemeAndHttpHost() . $ormUrl,
-//                    ];
-//                }
-//            }
-//        }
-//
-//        return $this->render('cms/sitemap/sitemap.xml.twig', [
-//            'sitemap' => $sitemap,
-//        ]);
-//    }
+    /**
+     * @route("/sitemap.xml")
+     * @return Response
+     */
+    public function sitemap()
+    {
+        $sitemap = [];
+        $request = Request::createFromGlobals();
+        $fullClass = ModelService::fullClass($this->connection, "Page");
+        $orms = $fullClass::active($this->connection, [
+            'whereSql' => '(m.hideFromWebNav != 1 OR m.hideFromWebNav IS NULL)'
+        ]);
+        foreach ($orms as $orm) {
+            $sitemap[] = [
+                'url' => $request->getSchemeAndHttpHost() . $orm->getUrl(),
+            ];
+        }
+
+        /** @var _Model[] $models */
+        $models = _Model::active($this->connection);
+        foreach ($models as $model) {
+            if ($model->getSiteMapUrl()) {
+                $url = $model->getSiteMapUrl();
+                $fullClass = ModelService::fullClass($this->connection, $model->getClassName());
+                $fields = array_keys($fullClass::getFields());
+                $orms = $fullClass::active($this->connection);
+                foreach ($orms as $orm) {
+                    $ormUrl = $url;
+                    foreach ($fields as $field) {
+                        $attr = 'get' . ucfirst($field);
+                        $replace = '{{' . $field . '}}';
+                        $value = $orm->$attr();
+                        $ormUrl = str_replace($replace, $value, $ormUrl);
+                    }
+                    $sitemap[] = [
+                        'url' => $request->getSchemeAndHttpHost() . $ormUrl,
+                    ];
+                }
+            }
+        }
+
+        return $this->render('cms/sitemap/sitemap.xml.twig', [
+            'sitemap' => $sitemap,
+        ]);
+    }
 
     /**
      * @route("/{page}", requirements={"page" = ".*"})
