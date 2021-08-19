368db44f1a4a79dd05866bc845db339146566404
john lennard <john@yakmoo.se>
2021-08-19 14:45:30 +1200

    Add in password validator constraint

--- Core/ORM/User.php
+++ Core/ORM/User.php
@@ -5,8 +5,29 @@ namespace MillenniumFalcon\Core\ORM;
 use MillenniumFalcon\Core\ORM\Traits\UserTrait;
 use Symfony\Component\Security\Core\User\EquatableInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Validator\Constraints as Assert;
 
 class User extends \MillenniumFalcon\Core\ORM\Generated\User implements UserInterface, EquatableInterface, \Serializable
 {
     use UserTrait;
+
+
+    /**
+     * @return mixed
+     * @Assert\Length(min=8)
+     * @Assert\NotCompromisedPassword()
+     */
+    public function getPasswordInput(): ?string
+    {
+        return parent::getPasswordInput();
+    }
+
+    /**
+     * @param mixed passwordInput
+     */
+    public function setPasswordInput($passwordInput): void
+    {
+        parent::setPasswordInput($passwordInput);
+    }
+
 }
