92be4c52608e7eef6ce2d01b86d34e37725de194
John Lennard <john@yakmoo.se>
2022-10-21 11:23:15 +1300

    Update AssetService.php

--- Core/Service/AssetService.php
+++ Core/Service/AssetService.php
@@ -164,7 +164,7 @@ class AssetService
             'image/png',
             'image/jpeg',
             'application/vnd.ms-powerpoint',
-            'application/vnd.openxmlformats-officedocument.presentationml.presentation  ',
+            'application/vnd.openxmlformats-officedocument.presentationml.presentation',
             'application/msword',
             'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
             'application/vnd.ms-excel',
