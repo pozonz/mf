bd4e0d5b659bfee7271154521016ad035f12a337
Chirag <chiragthakkar1802@gmail.com>
2023-05-22 04:02:34 +0000

    Allow Multiple dropdown for form

---
+++ FormDescriptor/Type/MultipleDropdownType.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace MillenniumFalcon\FormDescriptor\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class MultipleDropdownType extends ChoiceType
+{
+}
